118ae8e40a5790997501069dacb378815a9a3554
yuhao.yan <yuhao.yan@eunergy.com>
2021-07-25 05:25:40 +0800

    Initial commit

---
+++ CourseSelectionSystem/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseSelectionSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CourseQuery()
+        {
+            return View();
+        }
+        public ActionResult StudentQuery()
+        {
+            return View();
+        }
+
+        public ActionResult SelectionQuery()
+        {
+            return View();
+        }
+    }
+}
---
+++ CourseSelectionSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CourseSelectionSystem.MvcApplication" Language="C#" %>
